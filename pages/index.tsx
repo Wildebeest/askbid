0518514214513a87b80f3db1633b26a70ce00276
Matt Wilde <matthewcwilde@gmail.com>
2021-10-03 21:53:24 -0400

    Add code to connect to Phantom Wallets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,71 @@
 import Head from 'next/head';
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
+import {
+    PublicKey,
+} from "@solana/web3.js";
+
+function SearchButton(props) {
+    let hoverStates = "opacity-50";
+    if (props.isConnected) {
+        hoverStates = "hover:bg-green-400 hover:border-green-500";
+    }
+    return (
+        <button
+            className={"mr-3 bg-green-200 border border-green-300 py-3 px-4 rounded " + hoverStates}
+            disabled={!props.isConnected}>
+            Ask the internet
+        </button>
+    );
+}
+
+interface ConnectOpts {
+    onlyIfTrusted: boolean;
+}
+type PhantomEvent = "disconnect" | "connect";
+interface PhantomProvider {
+    isConnected: boolean | null;
+    on: (event: PhantomEvent, handler: (args: any) => void) => void;
+    connect: (opts?: Partial<ConnectOpts>) => Promise<{ publicKey: PublicKey }>;
+}
+
+const getProvider = (): PhantomProvider | undefined => {
+    if (!process.browser) {
+        return;
+    }
+    if ("solana" in window) {
+        const anyWindow: any = window;
+        const provider = anyWindow.solana;
+        if (provider.isPhantom) {
+            return provider;
+        }
+    }
+    window.open("https://phantom.app/", "_blank");
+};
+
+
+function WalletButton(props) {
+    const provider = getProvider();
+    useEffect(() => {
+        if (!provider) { return; }
+
+        provider.on("connect", () => {
+            props.setConnected(true);
+        });
+        provider.connect({onlyIfTrusted: true});
+    }, [provider]);
+
+    const onClick = () => {
+        provider.connect();
+    }
+
+    return (
+        <button
+            onClick={onClick}
+            className="mr-3 bg-purple-200 border border-purple-300 py-3 px-4 rounded hover:bg-purple-400 hover:border-purple-500">
+            Setup a wallet
+        </button>
+    );
+}
 
 export default function Home() {
     const [isConnected, setConnected] = useState<boolean>(false);
@@ -16,7 +82,7 @@ export default function Home() {
             </Head>
 
             <main>
-                <form onSubmit={submitSearch} className="flex justify-center pt-20">
+                <div className="flex justify-center pt-20">
                     <div>
                         <h1 className="mb-6 text-5xl">🚀 AskBid: A Search Exchange 🌚</h1>
 
@@ -27,13 +93,11 @@ export default function Home() {
                         </div>
 
                         <div className="mt-8 text-center">
-                            <button
-                                className="mr-3 bg-green-200 border border-green-300 py-3 px-4 rounded hover:bg-green-400 hover:border-green-500">
-                                Ask the internet
-                            </button>
+                            {!isConnected && <WalletButton setConnected={setConnected} />}
+                            <SearchButton isConnected={isConnected} setConnected={setConnected}/>
                         </div>
                     </div>
-                </form>
+                </div>
 
             </main>
         </div>
