03e0eaf8e58826f0e34828b29e4b3fbf83d7c61f
Matt Wilde <wilde@outlook.com>
2021-10-20 10:33:40 -0400

    Enable source maps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const {BugsnagBuildReporterPlugin, BugsnagSourceMapUploaderPlugin} = require("webpack-bugsnag-plugins");
 
 module.exports = {
+    productionBrowserSourceMaps: true,
     webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
         config.plugins.push(
             new BugsnagBuildReporterPlugin({
