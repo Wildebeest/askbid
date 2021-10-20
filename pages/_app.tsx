75c54efbd3d66edbac76e10010ed89f8109211d6
Matt Wilde <wilde@outlook.com>
2021-10-20 10:23:57 -0400

    The API key has to be public so that Bugsnag can write.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,15 @@ import Bugsnag from '@bugsnag/js';
 import BugsnagPluginReact from '@bugsnag/plugin-react';
 import React from 'react';
 
-if (process.env.BUGSNAG_API_KEY) {
+if (process.env.NEXT_PUBLIC_BUGSNAG_API_KEY) {
     Bugsnag.start({
-        apiKey: process.env.BUGSNAG_API_KEY,
+        apiKey: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY,
         plugins: [new BugsnagPluginReact()]
     });
 }
 
 function AskBidApp({Component, pageProps}) {
-    if (process.env.BUGSNAG_API_KEY) {
+    if (process.env.NEXT_PUBLIC_BUGSNAG_API_KEY) {
         const ErrorBoundary = Bugsnag.getPlugin('react')
             .createErrorBoundary(React);
         return (
